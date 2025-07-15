fa749473ec8a0568131842a1ad04f400b26dd2c6
Jens <107625279+Jensaboi@users.noreply.github.com>
2025-07-15 23:48:40 +0200

    Added win state
    -Confetti display on win
    -Restart game btn on win

--- src/App.jsx
+++ src/App.jsx
@@ -2,83 +2,103 @@ import React from "react";
 import CardButton from "./components/CardButton";
 import { allImages } from "./data";
 import { createRandomCardsArr } from "./utility/helper";
+import Confetti from "react-confetti";
 
 const FLIP_CARD_TIMER = 1000; //1sec
 
 function App() {
-    const [cards, setCards] = React.useState(() =>
-        createRandomCardsArr(allImages)
-    );
-    const [isBoardLocked, setBoardLocked] = React.useState(false);
-    const guessedCards = React.useRef([]);
-    const maximumGuesses = guessedCards.current.length === 2;
+  const [cards, setCards] = React.useState(() =>
+    createRandomCardsArr(allImages)
+  );
+  const [isBoardLocked, setBoardLocked] = React.useState(false);
+  const guessedCards = React.useRef([]);
+  const maximumGuesses = guessedCards.current.length === 2;
+  const isGameWon = cards.every((item) => item.open);
+  console.log(isGameWon);
 
-    function toggleCardOpen(card) {
-        setCards((prev) =>
-            prev.map((item) => {
-                if (item === card) {
-                    return { ...item, open: true };
-                }
-                return item;
-            })
-        );
-        if (!maximumGuesses) {
-            guessedCards.current.push(card);
+  function toggleCardOpen(card) {
+    setCards((prev) =>
+      prev.map((item) => {
+        if (item === card) {
+          return { ...item, open: true };
         }
+        return item;
+      })
+    );
+    if (!maximumGuesses) {
+      guessedCards.current.push(card);
     }
+  }
 
-    React.useEffect(() => {
-        let timeout;
-        if (maximumGuesses) {
-            setBoardLocked(true);
-            const [firstGuess, secondGuess] = guessedCards.current;
-            if (firstGuess.img !== secondGuess.img) {
-                timeout = setTimeout(() => {
-                    setCards((prev) =>
-                        prev.map((item) => {
-                            if (
-                                item.img === firstGuess.img ||
-                                item.img === secondGuess.img
-                            ) {
-                                return { ...item, open: false };
-                            }
-                            return item;
-                        })
-                    );
-                    guessedCards.current = [];
-                    setBoardLocked(false);
-                }, FLIP_CARD_TIMER);
-            } else {
-                timeout = setTimeout(() => {
-                    guessedCards.current = [];
-                    setBoardLocked(false);
-                }, FLIP_CARD_TIMER);
-            }
-        }
-        return () => clearTimeout(timeout);
-    }, [maximumGuesses]);
-    console.log(guessedCards.current);
-    return (
-        <>
-            <div className="w-full min-h-screen bg-[url(./assets/bg-img.jpg)] bg-cover bg-center overflow-hidden]">
-                <main className="w-full min-h-screen">
-                    <section className="container mx-auto min-h-screen flex flex-col justify-center items-center">
-                        <div className="bg-sky-400 border-3 border-blue-700 px-4 py-6 rounded-md grid grid-cols-4 gap-4 items-stretch justify-center">
-                            {cards.map((card, i) => (
-                                <CardButton
-                                    key={i}
-                                    className="border-2 sm:w-36 sm:h-36 border-pink-900 rounded-md cursor-pointer overflow-hidden bg-pink-400 hover:bg-pink-500 active:bg-pink-600"
-                                    props={card}
-                                    disabled={card.open || isBoardLocked}
-                                    onClick={() => toggleCardOpen(card)}
-                                />
-                            ))}
-                        </div>
-                    </section>
-                </main>
+  React.useEffect(() => {
+    let timeout;
+    if (maximumGuesses) {
+      setBoardLocked(true);
+      const [firstGuess, secondGuess] = guessedCards.current;
+      if (firstGuess.img !== secondGuess.img) {
+        timeout = setTimeout(() => {
+          setCards((prev) =>
+            prev.map((item) => {
+              if (item.img === firstGuess.img || item.img === secondGuess.img) {
+                return { ...item, open: false };
+              }
+              return item;
+            })
+          );
+          guessedCards.current = [];
+          setBoardLocked(false);
+        }, FLIP_CARD_TIMER);
+      } else {
+        timeout = setTimeout(() => {
+          guessedCards.current = [];
+          setBoardLocked(false);
+        }, FLIP_CARD_TIMER);
+      }
+    }
+    return () => clearTimeout(timeout);
+  }, [maximumGuesses]);
+
+  function restartGame() {
+    setCards(() => createRandomCardsArr(allImages));
+  }
+
+  return (
+    <>
+      <div className="w-full min-h-screen bg-[url(./assets/bg-img.jpg)] bg-cover bg-center overflow-hidden]">
+        <main className="w-full min-h-screen">
+          <section className="container mx-auto min-h-screen flex flex-col justify-center items-center">
+            {isGameWon && (
+              <Confetti
+                width={window.innerWidth}
+                height={window.innerHeight}
+                recycle={false}
+                numberOfPieces={600}
+              />
+            )}
+            <div className="bg-sky-400 border-3 border-blue-700 px-4 py-6 rounded-md grid grid-cols-4 gap-4 items-stretch justify-center mb-5">
+              {cards.map((card, i) => (
+                <CardButton
+                  key={i}
+                  className="border-2 sm:w-36 sm:h-36 border-pink-900 rounded-md cursor-pointer overflow-hidden bg-pink-400 hover:bg-pink-500 active:bg-pink-600"
+                  props={card}
+                  disabled={card.open || isBoardLocked}
+                  onClick={() => toggleCardOpen(card)}
+                />
+              ))}
             </div>
-        </>
-    );
+            {isGameWon && (
+              <button
+                onClick={restartGame}
+                className="bg-green-500 cursor-pointer rounded-md hover:bg-green-600 active:bg-green-700 text-zinc-50 text-lg font-medium px-6 py-3"
+              >
+                Play Again!
+              </button>
+            )}
+          </section>
+        </main>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/components/CardButton.jsx
+++ src/components/CardButton.jsx
@@ -1,20 +1,18 @@
 import pawIcon from "../assets/paw-bg.svg";
 export default function CardButton({ props, ...rest }) {
-    return (
-        <button {...rest}>
-            {props.open ? (
-                <img
-                    className="w-full h-full object-cover"
-                    src={
-                        new URL(`../assets/${props.img}`, import.meta.url).href
-                    }
-                    alt={props.img}
-                />
-            ) : (
-                <div className="w-full h-full flex flex-col justify-center items-center">
-                    <img src={pawIcon} className="w-full h-full object-cover" />
-                </div>
-            )}
-        </button>
-    );
+  return (
+    <button {...rest}>
+      {props.open ? (
+        <img
+          className="w-full h-full object-cover"
+          src={new URL(`../assets/${props.img}`, import.meta.url).href}
+          alt={props.img}
+        />
+      ) : (
+        <div className="w-full h-full flex flex-col justify-center items-center">
+          <img src={pawIcon} className="w-full h-full object-cover" />
+        </div>
+      )}
+    </button>
+  );
 }
